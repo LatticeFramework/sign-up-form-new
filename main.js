84ea4b7b3f8ef81efebace38aef5ec9eb13de8c6
LatticeFramework <10vv3cl@gmail.com>
2023-04-04 22:53:51 -0400

    Fix preventDefault() error

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const email = document.querySelector("#email");
 const phone = document.querySelector("#phone");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
-const errorMessage = document.querySelector("#errorMessage");
+const errorMessage = document.querySelector(".errorMessage");
 
-const inputs = (email, phone, password, confirmPassword);
+const inputs = [email, phone, password, confirmPassword];
 
 inputs.forEach((item) => {
     item.addEventListener("focusin", () => {
